0690115c729b6fe81fec6bde5144b042a2b62c88
David Maignan <davidmaignan@gmail.com>
2015-07-14 13:45:02 -0400

    initial commit

---
+++ src/main/java/org/main/JDriveMain.java
@@ -0,0 +1,12 @@
+package org.main;
+
+/**
+ * JDriveMain class
+ *
+ * David Maignan <davidmaignan@gmail.com>
+ */
+public class JDriveMain {
+    public static void main(String[] args) {
+        System.out.println("Hello JDrive");
+    }
+}
