30f8fb44f4484140eefa071439b494fdee52b76b
David Maignan <davidmaignan@gmail.com>
2015-07-15 09:06:20 -0400

    Drive service to connect to rest api

--- src/main/java/org/main/JDriveMain.java
+++ src/main/java/org/main/JDriveMain.java
@@ -1,12 +1,58 @@
 package org.main;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+import com.google.api.services.drive.Drive;
+import org.signin.DriveService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Arrays;
+
+
 /**
  * JDriveMain class
  *
  * David Maignan <davidmaignan@gmail.com>
  */
 public class JDriveMain {
-    public static void main(String[] args) {
-        System.out.println("Hello JDrive");
+
+    /**
+     * Main
+     * @param args
+     * @throws IOException
+     * @throws Throwable
+     */
+    public static void main(String[] args) throws IOException, Throwable {
+        // Build a new authorized API client service.
+        DriveService driveService = new DriveService();
+        Drive service = driveService.getDrive();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+                .setMaxResults(10)
+                .execute();
+        List<File> files = result.getItems();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getTitle(), file.getId());
+            }
+        }
     }
 }
