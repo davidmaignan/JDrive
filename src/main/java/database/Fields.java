480669993a8807ba2b209a683038b4118a541952
David Maignan <davidmaignan@gmail.com>
2015-12-23 22:37:30 -0500

    Add Change fields name

--- src/main/java/database/Fields.java
+++ src/main/java/database/Fields.java
@@ -1,19 +1,26 @@
 package database;
 
 /**
- * OrientDB fields name
+ * Database Node fields
  *
  * David Maignan <davidmaignan@gmail.com>
  */
 public class Fields {
-    public static final String ID            = "identifier";
-    public static final String TITLE         = "title";
-    public static final String PATH          = "path";
-    public static final String CREATED_DATE  = "createdDate";
-    public static final String MODIFIED_DATE = "modifiedDate";
-    public static final String MIME_TYPE     = "mimeType";
-    public static final String FILE          = "file";
-    public static final String PARENTS       = "parents";
-    public static final String IS_ROOT       = "IsRoot";
-    public static final String IS_TRASHED    = "isTrashed";
+    public static final String ID             = "identifier";
+    public static final String TITLE          = "title";
+    public static final String PATH           = "path";
+    public static final String CREATED_DATE   = "createdDate";
+    public static final String MODIFIED_DATE  = "modifiedDate";
+    public static final String MIME_TYPE      = "mimeType";
+    public static final String FILE           = "file";
+    public static final String PARENT_ID      = "parentId";
+    public static final String IS_ROOT        = "IsRoot";
+    public static final String IS_TRASHED     = "isTrashed";
+    public static final String LAST_CHANGE_ID = "lastChangeId";
+    public static final String IS_WRITTEN     = "isWrittern";
+
+    //Change
+    public static final String FILE_ID        = "fileId";
+    public static final String SELF_LINK      = "selfLink";
+    public static final String DELETED        = "deleted";
 }
