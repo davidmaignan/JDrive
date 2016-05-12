b6af969e899ed5655ff4687a88c14cfde0faa922
David Maignan <davidmaignan@gmail.com>
2016-05-12 10:02:13 -0400

    Clean Delete and trashed service

--- src/main/java/drive/change/services/DeleteService.java
+++ src/main/java/drive/change/services/DeleteService.java
@@ -24,53 +24,6 @@ public class DeleteService implements DriveChangeInterface {
 
     public final boolean execute(){
         return true;
-//        Path path = null;
-//
-//        try{
-//            path = FileSystems.getDefault().getPath(structure.getNewPath());
-//
-//            Files.deleteIfExists(path);
-//
-//            return true;
-//        } catch (FileNotFoundException exception) {
-//            return false;
-//        } catch (IOException exception) {
-////            exception.printStackTrace();
-//            logger.error(exception.getMessage());
-//            return false;
-//        }
     }
 
-    /**
-     * Delete files contains in directory prior deleting it
-     * @param path Path
-     * @throws IOException
-     */
-    private void deleteDirectory(Path path) throws IOException {
-        //Delete files
-        Files.list(path).filter( s -> {
-            if (Files.isDirectory(s)) return false;
-            else return true;
-        }).forEach( s -> {
-            try {
-                Files.deleteIfExists(s);
-            } catch (IOException e) {
-                logger.error("Error when deleting %s", path);
-            }
-        });
-
-        //If directory - delete recursively
-        Files.list(path).filter( s -> {
-            if (Files.isDirectory(s)) return true;
-            else return false;
-        }).forEach( s -> {
-            try {
-                deleteDirectory(s);
-            } catch (IOException e) {
-                logger.error("Error when deleting %s", path);
-            }
-        });
-
-        Files.deleteIfExists(path);
-    }
 }
--- src/main/java/drive/change/services/TrashedService.java
+++ src/main/java/drive/change/services/TrashedService.java
@@ -24,51 +24,6 @@ public class TrashedService implements DriveChangeInterface {
 
     public final boolean execute(){
         return true;
-//        try{
-//            Path path = FileSystems.getDefault().getPath(structure.getNewPath());
-//
-//            Files.deleteIfExists(path);
-//
-//            return true;
-//        } catch (FileNotFoundException exception) {
-//            return true;
-//        } catch (IOException exception) {
-////            exception.printStackTrace();
-//            logger.error(exception.getMessage());
-//            return false;
-//        }
-    }
 
-    /**
-     * Delete files contains in directory prior deleting it
-     * @param path Path
-     * @throws IOException
-     */
-    private void deleteDirectory(Path path) throws IOException {
-        //Delete files
-        Files.list(path).filter( s -> {
-            if (Files.isDirectory(s)) return false;
-            else return true;
-        }).forEach( s -> {
-            try {
-                Files.deleteIfExists(s);
-            } catch (IOException e) {
-                logger.error("Error when deleting %s", path);
-            }
-        });
-
-        //If directory - delete recursively
-        Files.list(path).filter( s -> {
-            if (Files.isDirectory(s)) return true;
-            else return false;
-        }).forEach( s -> {
-            try {
-                deleteDirectory(s);
-            } catch (IOException e) {
-                logger.error("Error when deleting %s", path);
-            }
-        });
-
-        Files.deleteIfExists(path);
     }
 }
