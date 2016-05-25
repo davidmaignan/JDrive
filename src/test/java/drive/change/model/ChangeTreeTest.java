54d0c5e50d5e95a509fa7578256d2d16f35f9988
David Maignan <davidmaignan@gmail.com>
2016-05-25 11:17:12 -0400

    Update ChangeTreeTest

--- src/test/java/drive/change/model/ChangeTreeTest.java
+++ src/test/java/drive/change/model/ChangeTreeTest.java
@@ -1,17 +1,10 @@
 package drive.change.model;
 
-import com.google.api.client.util.DateTime;
 import com.google.api.services.drive.model.Change;
 import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.ParentReference;
-import com.google.api.services.drive.model.User;
-import database.Fields;
-import database.RelTypes;
 import database.repository.ChangeRepository;
 import database.repository.FileRepository;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.graphdb.*;
 import org.slf4j.Logger;
@@ -42,7 +35,8 @@ public class ChangeTreeTest {
         ValidChange validChange = mock(ValidChange.class);
         Node fileNode = mock(Node.class);
         Change change = new Change();
-        change.setId(1000l);
+        File file = new File();
+        change.setFile(file);
 
         List<Node> trashedNode = mock(ArrayList.class);
 
@@ -58,8 +52,8 @@ public class ChangeTreeTest {
         changeTree.execute(changeList);
 
         verify(changeRepository, times(1)).addChange(change);
+        verify(fileRepository, never()).createIfNotExists(file);
     }
-
 
     @Test
     public void testExecuteNewFile() throws Exception {
@@ -70,7 +64,7 @@ public class ChangeTreeTest {
         Node newFileNode = mock(Node.class);
 
         Change change = new Change();
-        File file = this.generateFile("fileId", "parentId");
+        File file = new File();
         change.setFile(file);
 
         List<Node> trashedNode = mock(ArrayList.class);
@@ -83,8 +77,7 @@ public class ChangeTreeTest {
 
         //Create node
         when(fileRepository.getNodeById("parentId")).thenReturn(parentNode);
-        when(fileRepository.createNode(file)).thenReturn(newFileNode);
-        when(fileRepository.createParentRelation(newFileNode, parentNode)).thenReturn(true);
+        when(fileRepository.createIfNotExists(file)).thenReturn(true);
         when(validChange.getFileNode()).thenReturn(null);
         when(trashedNode.contains(newFileNode)).thenReturn(false);
         when(validChange.getChange()).thenReturn(change);
@@ -95,19 +88,17 @@ public class ChangeTreeTest {
     }
 
     @Test
-
     public void testExecuteTrashFile() throws Exception {
         List<ValidChange> changeList = mock(ArrayList.class);
         ValidChange validChange = mock(ValidChange.class);
         Node fileNode = mock(Node.class);
 
         Change change = new Change();
-        File file = this.generateFile("fileId", "parentId");
+        File file = new File();
         change.setFile(file);
 
         List<Node> trashedNode = mock(ArrayList.class);
         when(fileRepository.getTrashedList()).thenReturn(trashedNode);
-
 
         when(changeList.isEmpty()).thenReturn(false, true);
         when(changeList.remove(0)).thenReturn(validChange);
@@ -121,84 +112,5 @@ public class ChangeTreeTest {
 
         verify(changeRepository, times(1)).addChange(change);
         verify(fileRepository, times(1)).markAsUnTrashed(fileNode);
-
-    }
-
-    private File generateFile(String fileId, String parentId){
-        File file = new File();
-
-        file.setTitle(fileId);
-        file.setId(fileId);
-        file.setMimeType("application/vnd.google-apps.folder");
-        file.setCreatedDate(new DateTime("2015-01-07T15:14:10.751Z"));
-        file.setVersion(0l);
-
-        file.setParents(this.getParentReferenceList(
-                parentId, true
-        ));
-
-        return file;
-    }
-
-    private String getParentId(Node node) {
-        return node.getSingleRelationship(
-                RelTypes.PARENT, Direction.OUTGOING
-        ).getEndNode().getProperty(Fields.ID).toString();
-    }
-
-    private List<Change> getListOneChangeCreateFolder(){
-
-        List<Change> result = new ArrayList<>();
-
-        File folder1 = new File();
-
-        folder1.setTitle("folder4");
-        folder1.setId("folder4");
-        folder1.setMimeType("application/vnd.google-apps.folder");
-        folder1.setCreatedDate(new DateTime("2015-01-07T15:14:10.751Z"));
-        folder1.setVersion(0l);
-
-        folder1.setParents(this.getParentReferenceList(
-                "root", true
-        ));
-
-        Change change = new Change();
-        change.setId(0l);
-        change.setFileId(folder1.getId());
-        change.setFile(folder1);
-
-        result.add(change);
-
-        return result;
-    }
-
-    private ArrayList<ParentReference> getParentReferenceList(String id, boolean bool) {
-        ArrayList<ParentReference> parentList = new ArrayList<>();
-        parentList.add(this.getParentReference(id, bool));
-
-        return parentList;
-    }
-
-    private ParentReference getParentReference(String id, boolean bool) {
-        ParentReference parentReference = new ParentReference();
-        parentReference.setId(id);
-        parentReference.setIsRoot(bool);
-
-        return parentReference;
-    }
-
-    private ArrayList<User> getOwnerList(String displayName, boolean isAuthenticatedUser) {
-        ArrayList<User> ownerList = new ArrayList<>();
-        ownerList.add(this.getOwner(displayName, isAuthenticatedUser));
-
-        return ownerList;
-    }
-
-    private User getOwner(String displayName, boolean isAuthenticatedUser) {
-        User owner = new User();
-        owner.setDisplayName(displayName);
-        owner.setIsAuthenticatedUser(isAuthenticatedUser);
-
-        return owner;
     }
 }
