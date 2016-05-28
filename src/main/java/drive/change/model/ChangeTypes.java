2640fb3886701af4f061bc2fe3d319d23083487a
David Maignan <davidmaignan@gmail.com>
2016-05-28 11:36:12 -0400

    Add new change types: NEW_FILE, NEW_FOLDER, UNTRASHED

--- src/main/java/drive/change/model/ChangeTypes.java
+++ src/main/java/drive/change/model/ChangeTypes.java
@@ -5,5 +5,6 @@ package drive.change.model;
  * Created by David Maignan <davidmaignan@gmail.com> on 15-08-21.
  */
 public enum ChangeTypes {
-    DELETE, MOVE, VERSION, NULL, FILE_UPDATE, FOLDER_UPDATE, DOCUMENT, TRASHED;
+    DELETE, MOVE, VERSION, NULL, FILE_UPDATE, FOLDER_UPDATE,
+    DOCUMENT, TRASHED, NEW_FILE, NEW_FOLDER, UNTRASHED;
 }
