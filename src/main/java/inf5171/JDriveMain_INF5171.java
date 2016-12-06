0a58e98066a3d60f12e4dafa15fde3e350749621
David Maignan <davidmaignan@gmail.com>
2016-12-06 11:35:22 -0500

    Short test for main method to test execution for japet

--- src/main/java/inf5171/JDriveMain_INF5171.java
+++ src/main/java/inf5171/JDriveMain_INF5171.java
@@ -2,53 +2,98 @@ package inf5171;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.services.drive.model.File;
+import com.google.common.jimfs.Jimfs;
 import com.google.gson.GsonBuilder;
+import configuration.Configuration;
 import inf5171.deserializer.DateTimeDeserializer;
+import io.Folder;
+import io.filesystem.FileSystemWrapperTest;
 import model.tree.TreeBuilder;
+import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by david on 2016-12-01.
  */
 public class JDriveMain_INF5171 {
+
+    private static FileSystemWrapperTest fs;
+    private static Configuration configuration;
+
     public static void main(String args[]) throws IOException {
+
         List<File> list = new ArrayList<>();
 
-        for(inf5171.fixtures.File file : getDataSet("fixtures/files.json")){
-            list.add(setFile(file));
-        }
+
 
         TreeBuilder treeBuilder = new TreeBuilder("root");
         treeBuilder.build(list);
+
+        Configuration configuration = new Configuration();
+        fs = new FileSystemWrapperTest(configuration);
+
+
+        version1();
+
+//        Path data = fs.getPath(configuration.getRootFolder());
+//
+//        Files.list(data).forEach(file -> {
+//            try {
+//                System.out.println(String.format("%s", file));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
     }
 
-    protected static File setFile(inf5171.fixtures.File f){
-        File file = new File();
-        file.setId(f.id);
-        file.setName(f.name);
-        file.setMimeType(f.mimeType);
-        file.setTrashed(f.trashed);
-        file.setParents(f.parents);
-        file.setVersion(f.version);
-        file.setCreatedTime(f.createdTime);
-        file.setModifiedTime(f.modifiedTime);
+    private static void version1(){
+        ExecutorService pool = Executors.newFixedThreadPool(20);
 
-        return file;
+        int n = 10000;
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Thread threads[] = new Thread[n];
+        for( int i = 0; i < n; i++ ) {
+            int fi = i;
+            threads[i] = new Thread(
+                    () -> write(String.valueOf(fi)) );
+            threads[i].start();
+        }
+
+        for( int i = 0; i < n; i++ ) {
+            try { threads[i].join(); } catch( Exception e ){};
+        }
+
+        stopWatch.stop();
+
+        System.out.println(stopWatch.getNanoTime() / 1.0E-9);
+
     }
 
-    public static List<inf5171.fixtures.File> getDataSet(String filename) throws IOException {
-        GsonBuilder gson = new GsonBuilder();
-        gson.registerTypeAdapter(DateTime.class, new DateTimeDeserializer());
-        inf5171.fixtures.File[] fileList = gson.create().fromJson(new FileReader(
-                        JDriveMain_INF5171.class.getClassLoader().getResource(filename).getFile()),
-                inf5171.fixtures.File[].class
-        );
+    private static boolean write(String name){
+        try {
+            Thread.sleep((long)(Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Folder folder = new Folder(fs);
 
-        return Arrays.asList(fileList);
+        folder.setFileId(name);
+        return folder.write(name);
     }
+
+
+
 }
